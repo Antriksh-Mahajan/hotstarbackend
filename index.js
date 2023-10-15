fd80e0c14852c1c09054442e15f8107bc30fbade
Antriksh2000 <antrikshmahajan5@gmail.com>
2023-10-15 17:04:01 +0530

    packages added nodemon added

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const app = express();
 const port = 5000;
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("hotstar backend");
 });
+
+app.get("");
 
 app.listen(port, () => {
   console.log("server is listening on ", port);
